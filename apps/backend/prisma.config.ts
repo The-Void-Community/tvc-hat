bbd25d97a511b4d572d40d6e533d7e0a571b43aa
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2026-01-01 11:58:13 +0500

    -prettier

--- apps/backend/prisma.config.ts
+++ apps/backend/prisma.config.ts
@@ -1,12 +1,12 @@
-import 'dotenv/config'
-import { defineConfig, env } from 'prisma/config'
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: './src/database/schema.prisma',
+  schema: "./src/database/schema.prisma",
   migrations: {
-    path: './src/database/migrations',
+    path: "./src/database/migrations",
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url: env("DATABASE_URL"),
   },
-})
+});
