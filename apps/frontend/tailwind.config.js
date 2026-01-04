c5203028d0b00f6270f00c372570de72b1f5bbc2
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2026-01-04 13:53:06 +0500

    -prettier

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
 };
